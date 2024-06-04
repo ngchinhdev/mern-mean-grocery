bf9e4c3e01fc063901312a4cc9d10864e5acb1a8
sw-n2-9h <chinhnguyennn24@gmail.com>
2024-06-04 23:58:27 +0700

    dashboard first

--- frontend-angular/src/app/pages/admin/dashboard/dashboard.component.ts
+++ frontend-angular/src/app/pages/admin/dashboard/dashboard.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+import { IOrder } from '../../../core/models/order.model';
+import { IProduct } from '../../../core/models/products.model';
+import { OrderService } from '../../../core/services/order.service';
+import { ProductsService } from '../../../core/services/products.service';
+import { CurrencyPipe } from '../../../core/pipes/currency.pipe';
+import { DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, CurrencyPipe, DatePipe, RouterLink],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+
+export class DashboardComponent implements OnInit {
+  orders!: IOrder[];
+  revenue!: number;
+  products!: IProduct[];
+
+  orderService = inject(OrderService);
+  productService = inject(ProductsService);
+
+  ngOnInit(): void {
+    this.getAllOrders();
+    this.getAllProducts();
+  }
+
+  getAllOrders() {
+    this.orderService.getAllOrders().subscribe({
+      next: (res) => {
+        this.orders = res.data.sort((a, b) =>
+          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+        );
+        this.revenue = res.data.reduce((acc, cur) => acc + cur.totalPrice, 0);
+      }
+    });
+  }
+
+  getAllProducts() {
+    this.productService.getAllProducts().subscribe({
+      next: (res) => {
+        this.products = res.data;
+      }
+    });
+  }
+
 
 }
